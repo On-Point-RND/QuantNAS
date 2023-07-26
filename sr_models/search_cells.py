20203afe9e00e418eb06c76ceabca01a0e5682a4
xaosina <dima.tina2013@gmail.com>
2023-07-26 18:51:05 +0000

    new batch

--- sr_models/search_cells.py
+++ sr_models/search_cells.py
@@ -16,14 +16,14 @@ class Residual(nn.Module):
     def __init__(self, skip, body, c_out, skip_mode=True):
         super(Residual, self).__init__()
         self.skip = skip
-        self.cum_channels = nn.Conv2d((c_out // 2) * (len(body.net)), c_out, 1) #OPS["simple_1x1"]((c_out // 2) * (len(body.net)), c_out, [SUPPORT_CONV_BIT], None, 1, False, shared=True, quant_noise=False) 
+        self.cum_channels = OPS["simple_1x1"]((c_out // 2) * (len(body.net)), c_out, [SUPPORT_CONV_BIT], None, 1, False, shared=True, quant_noise=False) 
         self.body = body
         self.adn = ADN(c_out, skip_mode=skip_mode)
-        # self.esa = ESA(c_out, [8], shared=True)
+        self.esa = ESA(c_out, [8], shared=True)
 
     def forward(self, x, b_weights, s_weights):
         def func(x):
-            return self.body_split(x, b_weights, s_weights) 
+            return self.esa(self.body_split(x, b_weights, s_weights))
 
         return self.adn(x, func, x) 
 
@@ -202,10 +202,8 @@ class SearchArch(nn.Module):
         self.adn_one = ADN(36, skip_mode=skip_mode)
         self.adn_two = ADN(3, skip_mode=skip_mode)
 
-        self.c = nn.Conv2d(self.c_fixed * body_cells, self.c_fixed, 1, padding="same")
-        self.c2 = nn.Conv2d(self.c_fixed, self.c_fixed, 3, padding="same")
-        # self.c = OPS["simple_1x1"](self.c_fixed * body_cells, self.c_fixed, [SUPPORT_CONV_BIT], self.c_fixed, 1, False, shared=True, quant_noise=False) 
-        # self.c2 = OPS["simple_3x3"](self.c_fixed, self.c_fixed, [SUPPORT_CONV_BIT], self.c_fixed, 1, False, shared=True, quant_noise=False) 
+        self.c = OPS["simple_1x1"](self.c_fixed * body_cells, self.c_fixed, [SUPPORT_CONV_BIT], self.c_fixed, 1, False, shared=True, quant_noise=False) 
+        self.c2 = OPS["simple_3x3"](self.c_fixed, self.c_fixed, [SUPPORT_CONV_BIT], self.c_fixed, 1, False, shared=True, quant_noise=False) 
 
     def forward(self, x, alphas):
 
@@ -218,10 +216,8 @@ class SearchArch(nn.Module):
                 x = self.body[i](x, alphas["body"][i], alphas["skip"][i])
                 concat_skips += [x]
             concat_skips = torch.cat(concat_skips, dim=1)
-            x = torch.nn.functional.leaky_relu(self.c(concat_skips), negative_slope=0.05)
-            # x = self.c(concat_skips)
+            x = self.c(concat_skips)
             return self.c2(x)
-
         x = func_body(x) + head_skip
         x = self.pixel_up(self.upsample(x, alphas["upsample"]))
 
